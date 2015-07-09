5249489dafac04227b1184e85c17ce59f81b8142
Tim Stevens <ts_fj@yahoo.co.uk>
2015-07-09 15:50:35 +0100

    Sample Images
    Introduced some sample images, stats buttons and start of jQuery for
    switching content

---
+++ chartChanger.js
@@ -0,0 +1,15 @@
+// work out which stat button has been clicked
+			$(".glyphicon-stats").click (function(){
+				alert(this.id);
+			});
+
+		// $("iframe").attr("src", "http://www.ecowebhosting.co.uk");
+		// $("p").html("The text (and website) has been changed");
+		// alert($("iframe").attr("src"));
+	//});
+
+
+	//alert($("iframe").attr("src"));
+	// $("#greenCircle").hover(function(){
+	// 	alert("Hovering");
+	// });
