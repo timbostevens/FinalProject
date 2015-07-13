0be8f75d9ea5a95899cfcc8512510a58d5f4d4e8
Tim Stevens <ts_fj@yahoo.co.uk>
2015-07-13 11:19:38 +0100

    Clickable stat buttons
    Stat buttons in journeys page execute sample content changing

--- chartChanger.js
+++ chartChanger.js
@@ -1,6 +1,27 @@
 // work out which stat button has been clicked
 			$(".glyphicon-stats").click (function(){
-				alert(this.id);
+				switch(this.id){
+					case "distStat":
+						$("#squareChart").attr("src","img/sampleScatterGraphDist01.jpeg");
+					break;
+					case "durStat":
+						$("#squareChart").attr("src","img/sampleScatterGraphDuration01.jpeg");
+					break;
+					case "spdStat":
+						$("#squareChart").attr("src","img/sampleScatterGraphSpeed01.jpeg");
+					break;
+					case "enStat":
+						$("#squareChart").attr("src","img/sampleScatterGraphEnergy01.jpeg");
+					break;
+					case "co2Stat":
+						$("#squareChart").attr("src","img/sampleScatterGraphCO201.jpeg");
+					break;
+					default:
+					alert("Argh");
+
+				}
+
+
 			});
 
 		// $("iframe").attr("src", "http://www.ecowebhosting.co.uk");
