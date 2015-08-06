1f711d01bb35bd988c1d73500aa82499cb468b82
Tim Stevens <ts_fj@yahoo.co.uk>
2015-08-06 09:52:46 +0100

    Removed duplicate php code from visualisations

--- visualisations.php
+++ visualisations.php
@@ -1,21 +1,7 @@
 <!-- php script to generate stats -->
-<?php
-
-    /*
-    NEED SOME MANAGEMNET FOR DATABASE FAILURE
-    */
-    
-    // gets connection details
-    include("connection.php");
-    // sql query to count journeys
-    $query="SELECT COUNT(*) AS 'count' FROM jour";
-    // runs the query and sets to variable
-    $result = mysqli_query($connection,$query);
-    // gets the first row (all that is needed for this one)
-    $row = mysqli_fetch_array($result);
-    // extracts value from first row
-    $journeyCount=$row['count'];
-
+<?php    
+    // gets journey count and connection
+    include("journeyCount.php");
 ?>
 
 
