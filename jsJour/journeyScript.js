bfdae5cca4fa3bc2c54d8bfeb93f7ee2459d304a
Tim Stevens <ts_fj@yahoo.co.uk>
2015-09-13 21:51:04 +0100

    Twitter button nearly fixed

--- jsJour/journeyScript.js
+++ jsJour/journeyScript.js
@@ -81,14 +81,39 @@ $(".panel-group").on('show.bs.collapse',".panel-collapse", function() {
 
      // send the journey number to map loader
     load(journeyNumber, panelNumber);
+
     // call loading of column chart
     drawJourneyColumnChart(journeyNumber, panelNumber);
+    insertTwitter(journeyNumber, panelNumber);
 
 
     // set session storage var of panel number
     // sessionStorage.panelNumber = panelNumber;
     // sessionStorage.journeyNumber = journeyNumber;
 });
+
+
+/*
+Dynamically inserts tweet button
+*/
+function insertTwitter (journeyNumber, panelNumber){
+
+    // get vars
+     var date = document.getElementById("dateP"+panelNumber).innerHTML;
+     var distance = document.getElementById("distanceP"+panelNumber).innerHTML;
+
+
+// checks the button doesn't alreadt exist
+if($('#tweet-button'+panelNumber).length){
+  // do nothing
+} else {
+  // insert button with appropriaet text in correct place
+  // cross fingers
+  $("#facebook-button"+panelNumber).after('<iframe id="tweet-button'+panelNumber+'" allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.html?via=QUBDeLorean&amp;url='+WEBSITE_BASE_ADDRESS+'journeys.php?journey%3D'+journeyNumber+'&amp;text=The electric DeLorean Rides Again! '+distance+' on '+date+'&amp;count=none" style="width:70px; height:20px; float: right"></iframe>');
+}
+}
+
+
 
 /*
     * Loads map and sets default zoom
@@ -404,7 +429,7 @@ Adds the HTML for the number of panesl required
                 // add them
                         for (var loop = 0; loop < additionalPanelsRequired; loop++) {
                             // add the new panel after the current highest panel number
-                            $("#panel"+highestPanel).after("<div class='panel panel-default accordion-panel' id='panel"+panelNumber+"'> <div class='panel-heading' id='panel-heading"+panelNumber+"'> <h4 class='panel-title'> <a id='panelLink"+panelNumber+"' data-toggle='collapse' data-parent='#accordion' href='#collapse"+panelNumber+"'> <div class='row'> <div class='col-md-2 hidden-xs hidden-sm panel-title-expand'> <div id='expand-icon"+panelNumber+"' class='fa fa-angle-right fa-2x expand-symbol'></div> </div> <div class='col-md-7 panel-title-text'> <p class='accordion-title' id='journeyP"+panelNumber+"'>Journey x</p> <p id='dateP"+panelNumber+"'>Date x</p> <p id='startP"+panelNumber+"'>Start x</p> <p id='distanceP"+panelNumber+"'>x miles</p> </div> <div class='col-md-3 panel-title-map'> <img src='img/sampleMap02.jpg' id='panel-static-image"+panelNumber+"'/> </div> </div> </a> </h4> </div> <div id='collapse"+panelNumber+"' class='panel-collapse collapse'> <div class='panel-body'> <div class = 'journey-social-holder'> <a id='facebook-button"+panelNumber+"' style='float: right' href='"+WEBSITE_BASE_ADDRESS+"journeys.php' data-image='"+WEBSITE_BASE_ADDRESS+"img/qubev.png' data-title='The Electric DeLorean Rides Again!' data-desc='Some description k jh for this article' class='btnShare'><img src='img/FB-f-Logo__blue_58.png' alt='share' style='width:20px;height:20px;'></a> <iframe id='tweet-button"+panelNumber+"' allowtransparency='true' frameborder='0' scrolling='no'src='http://platform.twitter.com/widgets/tweet_button.html?via=QUBDeLorean&amp;text=Replace%20Me&amp;count=none'style='width:70px; height:20px; float: right'></iframe> <p>share this journey</p> </div> <div class = 'googlemap'> <div id='mapcanvas"+panelNumber+"' style='width: 100%; height: 300px;'></div> </div> <div id='journey-area-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> <div style='width: 100%; height:30px;'></div> <div class='row'> <div class='col-md-4 box'> <div style='width: 100%; height:30px;'></div> <p>Start Time: <span id='start-stat"+panelNumber+"'>xxxxxx</span></p> <p>End Time: <span id='end-stat"+panelNumber+"'>xxxxxx</span></p> <p> Distance: <span id='distance-stat"+panelNumber+"'>xxxxxx</span> miles</p> <p>Duration: <span id='duration-stat"+panelNumber+"'>xxxxxx</span> minutes</p> <p>Speed: <span id='speed-stat"+panelNumber+"'>xxxxxx</span> mph</p> <p>Petrol Saved: <span id='petrol-stat"+panelNumber+"'>xxxxxx</span> L</p> <p>CO2 Saved: <span id='co2-stat"+panelNumber+"'>xxxxxxx</span> kg</p> </div> <div class='col-md-8 box'> <div id='journey-column-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> </div> </div> </div> </div> </div>");    
+                            $("#panel"+highestPanel).after("<div class='panel panel-default accordion-panel' id='panel"+panelNumber+"'> <div class='panel-heading' id='panel-heading"+panelNumber+"'> <h4 class='panel-title'> <a id='panelLink"+panelNumber+"' data-toggle='collapse' data-parent='#accordion' href='#collapse"+panelNumber+"'> <div class='row'> <div class='col-md-2 hidden-xs hidden-sm panel-title-expand'> <div id='expand-icon"+panelNumber+"' class='fa fa-angle-right fa-2x expand-symbol'></div> </div> <div class='col-md-7 panel-title-text'> <p class='accordion-title' id='journeyP"+panelNumber+"'>Journey x</p> <p id='dateP"+panelNumber+"'>Date x</p> <p id='startP"+panelNumber+"'>Start x</p> <p id='distanceP"+panelNumber+"'>x miles</p> </div> <div class='col-md-3 panel-title-map'> <img src='img/sampleMap02.jpg' id='panel-static-image"+panelNumber+"'/> </div> </div> </a> </h4> </div> <div id='collapse"+panelNumber+"' class='panel-collapse collapse'> <div class='panel-body'> <div class = 'journey-social-holder'> <a id='facebook-button"+panelNumber+"' style='float: right' href='"+WEBSITE_BASE_ADDRESS+"journeys.php' data-image='"+WEBSITE_BASE_ADDRESS+"img/qubev.png' data-title='The Electric DeLorean Rides Again!' data-desc='Some description k jh for this article' class='btnShare'><img src='img/FB-f-Logo__blue_58.png' alt='share' style='width:20px;height:20px;'></a> <p>share this journey</p> </div> <div class = 'googlemap'> <div id='mapcanvas"+panelNumber+"' style='width: 100%; height: 300px;'></div> </div> <div id='journey-area-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> <div style='width: 100%; height:30px;'></div> <div class='row'> <div class='col-md-4 box'> <div style='width: 100%; height:30px;'></div> <p>Start Time: <span id='start-stat"+panelNumber+"'>xxxxxx</span></p> <p>End Time: <span id='end-stat"+panelNumber+"'>xxxxxx</span></p> <p> Distance: <span id='distance-stat"+panelNumber+"'>xxxxxx</span> miles</p> <p>Duration: <span id='duration-stat"+panelNumber+"'>xxxxxx</span> minutes</p> <p>Speed: <span id='speed-stat"+panelNumber+"'>xxxxxx</span> mph</p> <p>Petrol Saved: <span id='petrol-stat"+panelNumber+"'>xxxxxx</span> L</p> <p>CO2 Saved: <span id='co2-stat"+panelNumber+"'>xxxxxxx</span> kg</p> </div> <div class='col-md-8 box'> <div id='journey-column-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> </div> </div> </div> </div> </div>");    
                             // incremenet highest panel, panel number and while count
                             highestPanel++;
                             panelNumber++;
@@ -426,7 +451,7 @@ Adds the HTML for the number of panesl required
         // and the number of panels added is less than the max stated
         while((journeyCount>highestPanel) && (whileCount<=MAX_PANELS_TO_ADD)){
             // add the new panel after the current highest panel number
-            $("#panel"+highestPanel).after("<div class='panel panel-default accordion-panel' id='panel"+panelNumber+"'> <div class='panel-heading' id='panel-heading"+panelNumber+"'> <h4 class='panel-title'> <a id='panelLink"+panelNumber+"' data-toggle='collapse' data-parent='#accordion' href='#collapse"+panelNumber+"'> <div class='row'> <div class='col-md-2 hidden-xs hidden-sm panel-title-expand'> <div id='expand-icon"+panelNumber+"' class='fa fa-angle-right fa-2x expand-symbol'></div> </div> <div class='col-md-7 panel-title-text'> <p class='accordion-title' id='journeyP"+panelNumber+"'>Journey x</p> <p id='dateP"+panelNumber+"'>Date x</p> <p id='startP"+panelNumber+"'>Start x</p> <p id='distanceP"+panelNumber+"'>x miles</p> </div> <div class='col-md-3 panel-title-map'> <img src='img/sampleMap02.jpg' id='panel-static-image"+panelNumber+"'/> </div> </div> </a> </h4> </div> <div id='collapse"+panelNumber+"' class='panel-collapse collapse'> <div class='panel-body'> <div class = 'journey-social-holder'> <a id='facebook-button"+panelNumber+"' style='float: right' href='"+WEBSITE_BASE_ADDRESS+"journeys.php' data-image='"+WEBSITE_BASE_ADDRESS+"img/qubev.png' data-title='The Electric DeLorean Rides Again!' data-desc='Some description k jh for this article' class='btnShare'><img src='img/FB-f-Logo__blue_58.png' alt='share' style='width:20px;height:20px;'></a> <iframe id='tweet-button"+panelNumber+"' allowtransparency='true' frameborder='0' scrolling='no'src='http://platform.twitter.com/widgets/tweet_button.html?via=QUBDeLorean&amp;text=Replace%20Me&amp;count=none'style='width:70px; height:20px; float: right'></iframe> <p>share this journey</p> </div> <div class = 'googlemap'> <div id='mapcanvas"+panelNumber+"' style='width: 100%; height: 300px;'></div> </div> <div id='journey-area-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> <div style='width: 100%; height:30px;'></div> <div class='row'> <div class='col-md-4 box'> <div style='width: 100%; height:30px;'></div> <p>Start Time: <span id='start-stat"+panelNumber+"'>xxxxxx</span></p> <p>End Time: <span id='end-stat"+panelNumber+"'>xxxxxx</span></p> <p> Distance: <span id='distance-stat"+panelNumber+"'>xxxxxx</span> miles</p> <p>Duration: <span id='duration-stat"+panelNumber+"'>xxxxxx</span> minutes</p> <p>Speed: <span id='speed-stat"+panelNumber+"'>xxxxxx</span> mph</p> <p>Petrol Saved: <span id='petrol-stat"+panelNumber+"'>xxxxxx</span> L</p> <p>CO2 Saved: <span id='co2-stat"+panelNumber+"'>xxxxxxx</span> kg</p> </div> <div class='col-md-8 box'> <div id='journey-column-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> </div> </div> </div> </div> </div>");    
+            $("#panel"+highestPanel).after("<div class='panel panel-default accordion-panel' id='panel"+panelNumber+"'> <div class='panel-heading' id='panel-heading"+panelNumber+"'> <h4 class='panel-title'> <a id='panelLink"+panelNumber+"' data-toggle='collapse' data-parent='#accordion' href='#collapse"+panelNumber+"'> <div class='row'> <div class='col-md-2 hidden-xs hidden-sm panel-title-expand'> <div id='expand-icon"+panelNumber+"' class='fa fa-angle-right fa-2x expand-symbol'></div> </div> <div class='col-md-7 panel-title-text'> <p class='accordion-title' id='journeyP"+panelNumber+"'>Journey x</p> <p id='dateP"+panelNumber+"'>Date x</p> <p id='startP"+panelNumber+"'>Start x</p> <p id='distanceP"+panelNumber+"'>x miles</p> </div> <div class='col-md-3 panel-title-map'> <img src='img/sampleMap02.jpg' id='panel-static-image"+panelNumber+"'/> </div> </div> </a> </h4> </div> <div id='collapse"+panelNumber+"' class='panel-collapse collapse'> <div class='panel-body'> <div class = 'journey-social-holder'> <a id='facebook-button"+panelNumber+"' style='float: right' href='"+WEBSITE_BASE_ADDRESS+"journeys.php' data-image='"+WEBSITE_BASE_ADDRESS+"img/qubev.png' data-title='The Electric DeLorean Rides Again!' data-desc='Some description k jh for this article' class='btnShare'><img src='img/FB-f-Logo__blue_58.png' alt='share' style='width:20px;height:20px;'></a> <p>share this journey</p> </div> <div class = 'googlemap'> <div id='mapcanvas"+panelNumber+"' style='width: 100%; height: 300px;'></div> </div> <div id='journey-area-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> <div style='width: 100%; height:30px;'></div> <div class='row'> <div class='col-md-4 box'> <div style='width: 100%; height:30px;'></div> <p>Start Time: <span id='start-stat"+panelNumber+"'>xxxxxx</span></p> <p>End Time: <span id='end-stat"+panelNumber+"'>xxxxxx</span></p> <p> Distance: <span id='distance-stat"+panelNumber+"'>xxxxxx</span> miles</p> <p>Duration: <span id='duration-stat"+panelNumber+"'>xxxxxx</span> minutes</p> <p>Speed: <span id='speed-stat"+panelNumber+"'>xxxxxx</span> mph</p> <p>Petrol Saved: <span id='petrol-stat"+panelNumber+"'>xxxxxx</span> L</p> <p>CO2 Saved: <span id='co2-stat"+panelNumber+"'>xxxxxxx</span> kg</p> </div> <div class='col-md-8 box'> <div id='journey-column-chart"+panelNumber+"' style='width: 100%; height: 300px'></div> </div> </div> </div> </div> </div>");    
             // incremenet highest panel, panel number and while count
             highestPanel++;
             panelNumber++;
@@ -480,13 +505,13 @@ downloadUrl(urlGetData, function(dataResult) {
             document.getElementById("startP"+panelNumber).innerHTML = "Start: "+start;
             document.getElementById("distanceP"+panelNumber).innerHTML = distance+" miles";
             // create twitter message string
-            var twitterMessage = "Electric DeLorean Rides Again! "+distance+" miles on "+date;
+            //var twitterMessage = "Electric DeLorean Rides Again! "+distance+" miles on "+date;
             // get twitter button
-            var tweetButton = document.getElementById("tweet-button"+panelNumber);
+            //var tweetButton = document.getElementById("tweet-button"+panelNumber);
             // change text in the message
-            var newLink = WEBSITE_BASE_ADDRESS+"journeys.php?journey="+journeyID;
+            //var newLink = WEBSITE_BASE_ADDRESS+"journeys.php?journey="+journeyID;
             // add message and journey specific URL
-            tweetButton.src = tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(twitterMessage)+"&url="+encodeURIComponent(newLink));
+            //tweetButton.src = tweetButton.src.replace(/&text=[^&]+/, "&text=" + encodeURIComponent(twitterMessage)+"&url="+encodeURIComponent(newLink));
             // facebook button updated with custom text
             var facebookButton = document.getElementById("facebook-button"+panelNumber);
             facebookButton.setAttribute('data-desc', 'It travelled '+distance+' miles on '+date);
